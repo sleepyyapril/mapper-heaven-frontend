de30392d5bf338dcaab70507294b902e7e94caba
sleepyyapril <flyingkarii@gmail.com>
2026-09-08 23:58:33 -0300

    giggles

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,7 +6,7 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
-          <meta name="description" content="SolidStart with-auth example" />
+          <meta name="description" content="mapping heaven" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" type="image/svg" href="favicon.svg" />
           {assets}
