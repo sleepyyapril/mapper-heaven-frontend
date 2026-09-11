e1dca188aedaebb76bdb4664c446298e8a62ddb1
sleepyyapril <flyingkarii@gmail.com>
2026-09-11 20:01:07 -0300

    fix download

--- src/routes/maps.tsx
+++ src/routes/maps.tsx
@@ -30,7 +30,7 @@ export default function Maps() {
                     href={getLink(item)}
                     rel="external"
                     class="group w-full px-3 py-2 text-gray-100 bg-gray-800 border border-gray-700 rounded-lg hover:bg-[#5865F2] hover:border-none focus:outline-none transition-colors duration-300 flex items-center justify-center gap-2.5 text-gray-700 text-white"
-                    download={item}>
+                    download>
                       Download {item}
                   </a>
               }
